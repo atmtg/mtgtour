f200225d11d05a679f57e4af2d1699da5ea704f0
Tobias Anderberg <tobias.anderberg@agical.com>
2013-05-23 23:08:46 +0200

    Table now contains matchpoints

--- js/index.js
+++ js/index.js
@@ -9,15 +9,39 @@ define(['foliage', 'bud', 'phloem', 'lodash', 'foliage/foliage-event'], function
     playerStream.push(players);
   };
 
+  function matchPoints(results) {
+    return _.reduce(results, function(acc, val) {return acc + (val.win * 3) + val.draw}, 0);
+  };
+
   return f.div(
+    b.bus(function(bus) {
+            return f.div('#newplayer',
+                         f.input('#player_name', {'type': 'text', 
+                                                  'placeholder':'Player name', 
+                                                  'class':'span2',
+                                                  'style':'height:30px'}, bus.expose),
+                         f.button('Register', {'class':'btn'}, 
+                                  on.click(function(){addPlayer({name:bus.player_name(),
+                                                          results:[],
+                                                          resultStream:phloem.stream()})})))}),
     f.div('#players',
+          f.div('#players_header',
+                f.span('Player', {'class':'span2'}), 
+                f.span('Match points')),
           b.bind(playerStream.read,
                  function(players) {
                    return f.span(_.map(players, function(player) {
-                     return f.div(f.p(player.name))}
-                     ))}),
-          b.bus(function(bus) {
-            return f.div('#newplayer',
-                  f.input('#player_name', {type: 'text'}, bus.expose),
-                         f.button('new player', on.click(function(){addPlayer({name:bus.player_name()})})))})
-));});
+                     return f.div(f.span(player.name, {'class':'span2'}), 
+                                  f.span(b.bind(player.resultStream.read,
+                                                function(results){
+                                                  return f.span(matchPoints(results));
+                                                }
+                                                )),
+                                  f.button('add win', on.click(function(){
+                                    player.results = player.results.concat([{win:1, draw:0}]);
+                                    player.resultStream.push(player.results)})),
+                                  f.button('add draw', on.click(function(){
+                                    player.results = player.results.concat([{win:0, draw:1}]);
+                                    player.resultStream.push(player.results)}))
+                                 )}))})
+         ))})
