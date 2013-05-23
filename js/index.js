ddb3cc2307b441779a3f126641310c6abfe18a6d
Tobias Anderberg <tobias.anderberg@agical.com>
2013-05-23 17:06:23 +0200

    First version of adding players.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
-define(['foliage'], function(f) {
-    return f.p('hello world');
-});
+define(['foliage', 'bud', 'phloem', 'lodash', 'foliage/foliage-event'], function(f, b, phloem, _, on) {
+
+  var players = [];
+  var playerStream = phloem.stream();
+  playerStream.push(players);
+
+  function addPlayer(player) {
+    players = players.concat([player]);
+    playerStream.push(players);
+  };
+
+  return f.div(
+    f.div('#players',
+          b.bind(playerStream.read,
+                 function(players) {
+                   return f.span(_.map(players, function(player) {
+                     return f.div(f.p(player.name))}
+                     ))}),
+          b.bus(function(bus) {
+            return f.div('#newplayer',
+                  f.input('#player_name', {type: 'text'}, bus.expose),
+                         f.button('new player', on.click(function(){addPlayer({name:bus.player_name()})})))})
+));});
