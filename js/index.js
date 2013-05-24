634e18ff1bad04c13e70fe1648ccbafbc22e2c71
Tobias Anderberg <tobias.anderberg@agical.com>
2013-05-24 11:31:30 +0200

    MWP and GWP now calculated. Button to register player removed

--- js/index.js
+++ js/index.js
@@ -13,23 +13,43 @@ define(['foliage', 'bud', 'phloem', 'lodash', 'foliage/foliage-event'], function
     return _.reduce(results, function(acc, val) {
       if(val.wins > val.loss) return acc + 3;
       if(val.wins == val.loss) return acc + 1;
-      return acc}, 0)};
+      return acc}, 0)
+  };
+
+  function matchWinPercentage(results) {
+    var playersMatchPoints = matchPoints(results);
+    
+    return results ? (Math.max((playersMatchPoints/(results.length * 3)), 0.33) * 100).toFixed(2) : 0;
+  };
+
+  function gameWinPercentage(results) {
+    var winsAndTotal = _.reduce(results, function(acc, val) {
+      acc.wins += val.wins;
+      acc.total += val.wins + val.loss;
+      return acc}, {wins:0, total:0})
+
+    return winsAndTotal.total == 0 ? 0 : ((winsAndTotal.wins / winsAndTotal.total) * 100).toFixed(2);
+  };
 
   return f.div(
     b.bus(function(bus) {
             return f.div('#newplayer',
                          f.input('#player_name', {'type': 'text', 
-                                                  'placeholder':'Player name', 
-                                                  'class':'span2',
-                                                  'style':'height:30px'}, bus.expose),
-                         f.button('Register', {'class':'btn'}, 
-                                  on.click(function(){addPlayer({name:bus.player_name(),
-                                                          results:[],
-                                                          resultStream:phloem.stream()})})))}),
+                                                  'placeholder':'Player name'}, bus.expose,
+                                 on.keypress(function(event) {
+                                   if(event.keyCode === 13 || event.keyCode === 10) {
+                                     addPlayer({name:bus.player_name(),
+                                                results:[],
+                                                resultStream:phloem.stream()});
+                                     $('#player_name').select();
+                                   }})))}),
     f.div('#players',
           f.div('#players_header',
                 f.span('Player', {'class':'span2'}), 
-                f.span('Points')),
+                f.span('Points', {'class':'span1'}),
+                f.span('MWP', {'class':'span1'}),
+                f.span('GWP', {'class':'span1'}),
+                f.span('Actions')),
           b.bind(playerStream.read,
                  function(players) {
                    return f.span(_.map(players, function(player) {
@@ -37,7 +57,15 @@ define(['foliage', 'bud', 'phloem', 'lodash', 'foliage/foliage-event'], function
                                   f.span(b.bind(player.resultStream.read,
                                                 function(results){
                                                   return f.span(matchPoints(results));
-                                                })),
+                                                }), {'class':'span1'}),
+                                  f.span(b.bind(player.resultStream.read,
+                                                function(results){
+                                                  return f.span(matchWinPercentage(results));
+                                                }), {'class':'span1'}),
+                                  f.span(b.bind(player.resultStream.read,
+                                                function(results){
+                                                  return f.span(gameWinPercentage(results));
+                                                }), {'class':'span1'}),
                                   f.button('2-0', on.click(function(){
                                     player.results = player.results.concat([{wins:2, loss:0}]);
                                     player.resultStream.push(player.results)})),
