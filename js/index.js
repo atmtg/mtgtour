c246836218788c7c0b0e94cba116666769c75778
Tobias Anderberg <tobias.anderberg@agical.com>
2013-05-25 20:31:03 +0200

    Bye now generates matchwin on pairing, and buttonpanel is never shown for tables with bye.

--- js/index.js
+++ js/index.js
@@ -44,43 +44,71 @@ define(['foliage',
     return winsAndTotal.total == 0 ? 0 : ((winsAndTotal.wins / winsAndTotal.total) * 100).toFixed(2);
   };
          
-  function registerMatchResult(player1, player2, player1Games, player2Games) {
+  function registerMatchResult(player1, player2, player1Games, player2Games, reportStream) {
     player1.results = player1.results.concat([{wins:player1Games, loss:player2Games}]);
     player1.resultStream.push(player1.results)
-    player2.results = player2.results.concat([{wins:player2Games, loss:player1Games}]);
-    player2.resultStream.push(player2.results)
+    if(player2) {
+      player2.results = player2.results.concat([{wins:player2Games, loss:player1Games}]);
+      player2.resultStream.push(player2.results)
+      
+      if(player1Games == player2Games) 
+        reportStream.push('Draw');
+      if(player1Games > player2Games) 
+        reportStream.push(player1.name + ' wins <br>' + player1Games + ' - ' + player2Games);
+      if(player2Games > player1Games)
+        reportStream.push(player2.name + ' wins <br>' + player2Games + ' - ' + player1Games);
+    } else {
+      reportStream.push(player1.name + ' receives a bye');
+    }
   };
+
+  function opponentName(player1, player2, match) {
+    if(player2)
+      return player2.name;
+    else {
+      registerMatchResult(player1, player2, 2, 0, match.reportStream);
+      return '- Bye -';
+    }
+  }
 
   function createMatchTables(matches) {
     var tableCount = 1;
     return f.div('#matchboard', _.map(matches, function(match) {
-      var player1 = match[0];
-      var player2 = match[1];
+      var player1 = match.player1;
+      var player2 = match.player2;
       return f.div('#table' + tableCount++, {'class':'matchtable'},
                    on.click(function(){
-                     $(this).find('.buttonPanel').fadeToggle();
+                     if(player2) {
+                       $(this).find('.buttonPanel').fadeToggle();
+                     }
                    }),
                    f.div({'class':'matchTableSurface'}),
                    f.p(player1.name, {'class':'playerName'}),
-                   f.p(player2 ? player2.name : '-- Bye --', {'class':'player2 playerName'}),
+                   f.p(opponentName(player1, player2, match), {'class':'player2 playerName'}),
+                   b.bind(match.reportStream.read, function(report) {
+                     return f.div({'class':'matchResult'}, 
+                                 f.span(report));
+                   }),
                    f.div({'class':'buttonPanel', 'style':'display:none'},
                          f.button('2-0', {'class':'btn'}, on.click(function(){
-                           registerMatchResult(player1, player2, 2, 0);})),
+                           registerMatchResult(player1, player2, 2, 0, match.reportStream);})),
                          f.button('2-1', {'class':'btn'}, on.click(function(){
-                           registerMatchResult(player1, player2, 2, 1);})),
+                           registerMatchResult(player1, player2, 2, 1, match.reportStream);})),
                          f.button('1-1', {'class':'btn'}, on.click(function(){
-                           registerMatchResult(player1, player2, 1, 1);})),
+                           registerMatchResult(player1, player2, 1, 1, match.reportStream);})),
                          f.button('1-2', {'class':'btn'}, on.click(function(){
-                           registerMatchResult(player1, player2, 1, 2);})),
+                           registerMatchResult(player1, player2, 1, 2, match.reportStream);})),
                          f.button('0-2', {'class':'btn'}, on.click(function(){
-                           registerMatchResult(player1, player2, 0, 2);}))
+                           registerMatchResult(player1, player2, 0, 2, match.reportStream);}))
                         ))}))};
 
   function pairForRoundOne(players) {
     var firstHalf = players.slice(0,Math.ceil(players.length/2));
     var secondHalf = players.slice(Math.ceil(players.length/2), players.length);
-    var pairing = _.zip(firstHalf, secondHalf);
-    matches = pairing;
+    var pairings = _.zip(firstHalf, secondHalf);
+    matches = _.map(pairings, function(pairing) {
+      return {player1:pairing[0], player2:pairing[1], reportStream:phloem.stream()};
+    });
     matchStream.push(matches);
   }
   
