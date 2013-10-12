e08d12c89a70ffe976d5280f67bc3a3f269b3c86
Tobias Anderberg <tobias.anderberg@agical.com>
2013-10-12 22:05:19 +0200

    Only one button panel a time visible. Some tests failing.

--- js/matchtable.js
+++ js/matchtable.js
@@ -2,91 +2,98 @@ define(['foliage',
         'foliage/foliage-event',
         'bud'],
        function(f, 
-                on, 
-                b) {
-           var opponentName = function (player2) {
-               return player2 ? player2.name : '- Bye -';
-           };
-
-           function onClickSelectOrMove(matchStream, matches, match, playerIndex) {
-               return on.click(function() {
-                   if(!roundTimerRunning()) {
-                       if(playerSelected) {
-                           var tempPlayer = match.players[playerIndex];
-                           match.players[playerIndex] = playerSelected.theMatch.players[playerSelected.thePlayerIndex];
-                           playerSelected.theMatch.players[playerSelected.thePlayerIndex] = tempPlayer;
-                           
-                           cleanUpMatch(matches, match);
-                           cleanUpMatch(matches, playerSelected.theMatch);
-                           
-                           playerSelected = undefined;
-                           matchStream.push(matches);
-                       } else {
-                           playerSelected = {theMatch:match, thePlayerIndex:playerIndex};
-                       } 
-                       $(this).toggleClass('selected');
-                   }
-               });
-           };
-
-           return function(matchStream, matches, match, roundTimerRunning, tooltip) {
-               var player1 = match.players[0];
-               var player2 = match.players[1];
-               var currentResult = {games1:0, games2:0};
-               return f.div('#table', {'class':'matchtable span3'},
-                            tooltip('Click Table to Register Match Result. \nTo Adjust Pairing: Click a Player Name to Select that Player, and then another Player Name to Switch Chairs.'),
-                            f.div(f.div('.matchTableSurface'),
-                                  f.div('.player1Side', on.click(function() {
-                                    if(player2 && roundTimerRunning()) {
-                                      $(this).parents('#table').find('.topButtonPanel').fadeToggle();}
-                                  }), f.p('.player1 playerName', player1.name, 
-                                          onClickSelectOrMove(matchStream, matches, match, 0))),
-                                  f.div('.noMansLand', on.click(function() {
-                                    if(player2 && roundTimerRunning()) {
-                                      $(this).parents('#table').find('.rightButtonPanel').fadeToggle();}
-                                  }), b.bind(match.reportStream.read, function(results) {
-                                    var reportString = "";
-                                    
-                                    if(results) {
-                                      currentResult = results;
-                                      reportString =  results.games1 + ' - ' + 
-                                        results.games2 + 
-                                        (results.draws  ? ' - ' + results.draws : '');
-                                    }
-                                    return f.div('.matchResult', 
-                                                 f.span(reportString));
-                                    
-                                  })),
-                                  f.div('.player2Side', on.click(function() {
-                                    if(player2 && roundTimerRunning()) {
-                                      $(this).parents('#table').find('.bottomButtonPanel').fadeToggle();
-                                    }
-                                  }), f.p('.player2 playerName', opponentName(player2), 
-                                            onClickSelectOrMove(matchStream, matches, match, 1)))),
-                            f.div('.topButtonPanel', {'style':'display:none'},
-                                  f.button('.btn', '2-0', on.click(function(){
-                                    match.registerResult( 2, 0);})),
-                                  f.button('.btn', '2-1', on.click(function(){
-                                    match.registerResult( 2, 1);})),
-                                  f.button('.btn', '1-0', on.click(function(){
-                                    match.registerResult( 1, 0);}))),
-                            f.div('.rightButtonPanel', {'style':'display:none'}, 
-                                  f.button('.btn', '0-0-z', on.click(function(){
-                                    match.registerResult( 0, 0);})),
-                                  f.button('.btn', '1-1-z', on.click(function(){
-                                    match.registerResult( 1, 1);})),
-                                  f.button('.btn', 'x-y-1', on.click(function(){
-                                    match.registerDraw(1);})),
-                                  f.button('.btn', 'x-y-2', on.click(function(){
-                                    match.registerDraw(2);})),
-                                  f.button('.btn', 'x-y-3', on.click(function(){
-                                    match.registerDraw(3);}))),
-                            f.div('.bottomButtonPanel', {'style':'display:none'},
-                                  f.button('.btn', '0-1', on.click(function(){
-                                    match.registerResult( 0, 1);})),
-                                  f.button('.btn', '1-2', on.click(function(){
-                                    match.registerResult( 1, 2);})),
-                                  f.button('.btn', '0-2', on.click(function(){
-                                    match.registerResult( 0, 2);}))));
-           };
-       });
+         on, 
+         b) {
+         var opponentName = function (player2) {
+           return player2 ? player2.name : '- Bye -';
+         };
+         
+         function selectOrMove(matchStream, matches, match, playerIndex) {
+           return function() {
+             if(playerSelected) {
+               var tempPlayer = match.players[playerIndex];
+               match.players[playerIndex] = playerSelected.theMatch.players[playerSelected.thePlayerIndex];
+               playerSelected.theMatch.players[playerSelected.thePlayerIndex] = tempPlayer;
+               
+               cleanUpMatch(matches, match);
+               cleanUpMatch(matches, playerSelected.theMatch);
+               
+               playerSelected = undefined;
+               matchStream.push(matches);
+             } else {
+               playerSelected = {theMatch:match, thePlayerIndex:playerIndex};
+             } 
+             $(this).toggleClass('selected');               
+           }};
+         
+         
+         return function(matchStream, matches, match, roundTimerRunning, tooltip, complete) {
+           var player1 = match.players[0];
+           var player2 = match.players[1];
+           var currentResult = {games1:0, games2:0};
+           return f.div('#table', {'class':'matchtable span3'},
+                        tooltip('Click Table to Register Match Result. \nTo Adjust Pairing: Click a Player Name to Select that Player, and then another Player Name to Switch Chairs.'),
+                        f.div(f.div('.matchTableSurface'),
+                              f.div('.player1Side', on.click(function() {
+                                if(player2 && roundTimerRunning()) {
+                                  $(this).parents('#table').find('.rightButtonPanel').fadeOut();
+                                  $(this).parents('#table').find('.bottomButtonPanel').fadeOut();
+                                  $(this).parents('#table').find('.topButtonPanel').fadeToggle();
+                                } else {
+                                  selectOrMove(matchStream, matches, match, 0)();
+                                }
+                              }), f.p('.player1 playerName', player1.name)),
+                              f.div('.noMansLand', on.click(function() {
+                                if(player2 && roundTimerRunning()) {
+                                  $(this).parents('#table').find('.topButtonPanel').fadeOut();
+                                  $(this).parents('#table').find('.bottomButtonPanel').fadeOut();
+                                  $(this).parents('#table').find('.rightButtonPanel').fadeToggle();}
+                              }), b.bind(match.reportStream.read, function(results) {
+                                var reportString = "";
+                                
+                                if(results) {
+                                  currentResult = results;
+                                  reportString =  results.games1 + ' - ' + 
+                                    results.games2 + 
+                                    (results.draws  ? ' - ' + results.draws : '');
+                                }
+                                return f.div('.matchResult', 
+                                             f.span(reportString));
+                                
+                              })),
+                              f.div('.player2Side', on.click(function() {
+                                if(player2 && roundTimerRunning()) {
+                                  $(this).parents('#table').find('.topButtonPanel').fadeOut();
+                                  $(this).parents('#table').find('.rightButtonPanel').fadeOut();
+                                  $(this).parents('#table').find('.bottomButtonPanel').fadeToggle();
+                                } else if (player2){
+                                  selectOrMove(matchStream, matches, match, 1);
+                                }
+                              }), f.p('.player2 playerName', opponentName(player2)))),
+                        f.div('.topButtonPanel', {'style':'display:none'},
+                              f.button('.btn', '2-0', on.click(function(){
+                                match.registerResult( 2, 0);})),
+                              f.button('.btn', '2-1', on.click(function(){
+                                match.registerResult( 2, 1);})),
+                              f.button('.btn', '1-0', on.click(function(){
+                                match.registerResult( 1, 0);}))),
+                        f.div('.rightButtonPanel', {'style':'display:none'}, 
+                              f.button('.btn', '0-0-z', on.click(function(){
+                                match.registerResult( 0, 0);})),
+                              f.button('.btn', '1-1-z', on.click(function(){
+                                match.registerResult( 1, 1);})),
+                              f.button('.btn', 'x-y-1', on.click(function(){
+                                match.registerDraw(1);})),
+                              f.button('.btn', 'x-y-2', on.click(function(){
+                                match.registerDraw(2);})),
+                              f.button('.btn', 'x-y-3', on.click(function(){
+                                match.registerDraw(3);}))),
+                        f.div('.bottomButtonPanel', {'style':'display:none'},
+                              f.button('.btn', '0-1', on.click(function(){
+                                match.registerResult( 0, 1);})),
+                              f.button('.btn', '1-2', on.click(function(){
+                                match.registerResult( 1, 2);})),
+                              f.button('.btn', '0-2', on.click(function(){
+                                match.registerResult( 0, 2);}))));
+         };
+       })
--- test/matchtable-spec.js
+++ test/matchtable-spec.js
@@ -1,96 +1,120 @@
 define(
-    ['matchtable', 
-     'jquery',
-     'foliage',
-     'phloem',
-    'bud',
-    'lodash'],
-    function( matchtable, 
-             $,
-             f,
-             phloem,
-             b,
-             _) {
+  ['matchtable', 
+   'jquery',
+   'foliage',
+   'phloem',
+   'bud',
+   'lodash'],
+  function( matchtable, 
+     $,
+     f,
+     phloem,
+     b,
+     _) {
+    
+    var assert = buster.assert;
+    
+    function tooltip() {
+      return f.div();
+    }
+    
+    function setUpTable(match, running, complete) {
+      var parent = $('<div />');
+      matchtable(undefined, undefined, match, function(){return running;}, tooltip, complete)(parent);
+      return parent;
+    }
+    
+    function resultPresentation(result, expectedPresentation) {
+      return function(done) {
+        var reportStream = phloem.stream();
+        var match =  {
+          players: [{name: 'Marshall'}, {name: 'Brian'}],
+          reportStream: reportStream
+        };
         
-        var assert = buster.assert;
+        var tableParent = setUpTable(match, false);
+        reportStream.push(result);
         
-        function tooltip() {
-            return f.div();
-        }
-
-        function setUpTable(match, running) {
-            var parent = $('<div />');
-            matchtable(undefined, undefined, match, function(){return running;}, tooltip)(parent);
-            return parent;
-        }
-
-        function resultPresentation(result, expectedPresentation) {
-            return function(done) {
-                var reportStream = phloem.stream();
-                var match =  {
-                    players: [{name: 'Marshall'}, {name: 'Brian'}],
-                    reportStream: reportStream
-                };
-
-                var tableParent = setUpTable(match, false);
-                reportStream.push(result);
-
-                phloem.each(reportStream.read, function(val) {
-                    if(val) {
-                        assert.equals($('.matchResult span', tableParent).text().trim(), expectedPresentation);
-                        done();
-                    }
-                });
-            }
-        }
-
-        buster.testCase('matchtable', {
-          'when player2 is absent player1 receives bye' : function() {
-            var match =  {
-              players: [{name: 'Marshall'}, undefined],
-              reportStream: phloem.stream()
-            };
-            
-            var tableParent = setUpTable(match, false);
-            
-            assert.equals($('.player2', tableParent).text().trim(), '- Bye -');
-          },
-          'presents scores when player1 wins and no draws' : resultPresentation({games1: 2, games2: 1}, '2 - 1'),
-          'presents scores when tied game and no draws' : resultPresentation({games1: 1, games2: 1}, '1 - 1'),
-          'presents scores when player2 wins and no draws' : resultPresentation({games1: 1, games2: 2}, '1 - 2'),
-          'presents scores and draws when draws' : resultPresentation({games1: 1, games2: 1, draws:1}, '1 - 1 - 1')
+        phloem.each(reportStream.read, function(val) {
+          if(val) {
+            assert.equals($('.matchResult span', tableParent).text().trim(), expectedPresentation);
+            done();
+          }
         });
-      buster.testCase('clicking', {
-        'middle of matchtable shows right buttonpanel' : function() {  
-          var match = {
-            players: [{name: 'Marshall'}, {name: 'Bwonger'}],
-            reportStream: phloem.stream()
-          };
-          var tableParent = setUpTable(match, true);
-
-          $('.noMansLand', tableParent).trigger('click');
-          assert.equals($('.rightButtonPanel', tableParent).css('display'), 'block');
-        },
-        'player2s part of table shows bottom buttonpanel' : function() {
-          var match = {
-            players: [{name: 'Marshall'}, {name: 'Bwonger'}],
-            reportStream: phloem.stream()
-          };
-          var tableParent = setUpTable(match, true);
-
-          $('.player2Side', tableParent).trigger('click');
-          assert.equals($('.bottomButtonPanel', tableParent).css('display'), 'block');
-        },
-        'player1s part of table shows top buttonpanel' : function() {
-          var match = {
-            players: [{name: 'Marshall'}, {name: 'Bwonger'}],
-            reportStream: phloem.stream()
-          };
-          var tableParent = setUpTable(match, true);
-
-          $('.player1Side', tableParent).trigger('click');
-          assert.equals($('.topButtonPanel', tableParent).css('display'), 'block');
-        }
-      });
+      }
+    }
+      
+    function twoPlayerRunningMatchTable() {
+      var match = {
+        players: [{name: 'Marshall'}, {name: 'Bwonger'}],
+        reportStream: phloem.stream()
+      };
+      return setUpTable(match, true);
+    }
+      
+    buster.testCase('matchtable', {
+      'when player2 is absent player1 receives bye' : function() {
+        var match =  {
+          players: [{name: 'Marshall'}, undefined],
+          reportStream: phloem.stream()
+        };
+        
+        var tableParent = setUpTable(match, false);
+        
+        assert.equals($('.player2', tableParent).text().trim(), '- Bye -');
+      },
+      'presents scores when player1 wins and no draws' : resultPresentation({games1: 2, games2: 1}, '2 - 1'),
+      'presents scores when tied game and no draws' : resultPresentation({games1: 1, games2: 1}, '1 - 1'),
+      'presents scores when player2 wins and no draws' : resultPresentation({games1: 1, games2: 2}, '1 - 2'),
+      'presents scores and draws when draws' : resultPresentation({games1: 1, games2: 1, draws:1}, '1 - 1 - 1')
     });
+    buster.testCase('clicking', {
+      'middle of matchtable shows right buttonpanel' : function() {  
+        var tableParent = twoPlayerRunningMatchTable();
+        
+        $('.noMansLand', tableParent).trigger('click');
+        assert.equals($('.rightButtonPanel', tableParent).css('display'), 'block');
+      },
+      'player2s part of table shows bottom buttonpanel' : function() {
+        var tableParent = twoPlayerRunningMatchTable();
+        
+        $('.player2Side', tableParent).trigger('click');
+        assert.equals($('.bottomButtonPanel', tableParent).css('display'), 'block');
+      },
+      'player1s part of table shows top buttonpanel' : function() {
+        var tableParent = twoPlayerRunningMatchTable();
+        
+        $('.player1Side', tableParent).trigger('click');
+        assert.equals($('.topButtonPanel', tableParent).css('display'), 'block');
+      },
+      'another area of the table hides the other buttonPanels' : function(done) {
+        this.timeout = 1000;
+        var tableParent = twoPlayerRunningMatchTable();
+        
+        $('.topButtonPanel', tableParent).show();
+        $('.player2Side', tableParent).trigger('click');
+        setTimeout(function(){done(assert.equals($('.topButtonPanel', tableParent).css('display'), 'none'))}, 1000);
+        //$('.player1Side', tableParent).trigger('click');
+        //setTimeout(function(){assert.equals($('.bottomButtonPanel', tableParent).css('display'), 'none');}, 1000);
+        //$('.noMansLand', tableParent).trigger('click');
+        //setTimeout(function(){assert.equals($('.topButtonPanel', tableParent).css('display'), 'none');}, 1000);
+        //$('.player1Side', tableParent).trigger('click');
+        //setTimeout(function(){assert.equals($('.rightButtonPanel', tableParent).css('display'), 'none');}, 1000);
+        //$('.noMansLand', tableParent).trigger('click');
+        //$('.player2Side', tableParent).trigger('click');
+        //setTimeout(function(){assert.equals($('.rightButtonPanel', tableParent).css('display'), 'none');}, 1000);
+        //$('.noMansLand', tableParent).trigger('click');
+        //setTimeout(function(){assert.equals($('.bottomButtonPanel', tableParent).css('display'), 'none');}, 1000);
+      },
+      'player1side when round not running will select player1' : function() {
+        var match = {
+          players: [{name: 'Marshall'}, {name: 'Bwonger'}],
+          reportStream: phloem.stream()
+        };
+        var tableParent = setUpTable(match, false);
+        $('.player1Side', tableParent).trigger('click');
+        assert.isTrue($('.player1', tableParent).hasClass('selected'));
+      }
+    });
+  });
 
